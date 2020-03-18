f462268f57635554c3c634daa64f84d9181686c6
unknown <12shirel@gmail.com>
2020-03-18 12:32:00 +0200

    test_valid_postal_code_invalid_country_valid_username

--- postal_code_test.py
+++ postal_code_test.py
@@ -71,5 +71,32 @@ class MyTestCase(unittest.TestCase):
         # assert
         self.assertEqual(result, expected)
 
+    @patch('src.postal_code_class.requests.get')
+    def test_valid_postal_code_invalid_country_valid_username(self, mock_get):
+        '''
+
+        :param mock_get: mock the object behaviour
+        :return:
+        '''
+        postal_code_information = {'postalcodes': []}
+
+        # Configure the mock to return a response with an OK status code. Also, the mock should have
+        # a `json()` method that returns.
+        mock_get.return_value = Mock(ok=True)
+        mock_get.return_value.json.return_value = postal_code_information
+
+        # assume
+        postal_code = '6600'
+        country = 'HELLO'
+        user_name = 'shirel_biton'
+
+        # expected
+        expected = {'postalcodes': []}
+
+        # action
+        result = PostalCode.postal_code_places(postal_code, country, user_name)
+
+        # assert
+        self.assertEqual(result, expected)
 if __name__ == '__main__':
     unittest.main()
